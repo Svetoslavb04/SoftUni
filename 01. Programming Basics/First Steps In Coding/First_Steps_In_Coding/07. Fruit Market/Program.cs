63fe9a9a060068a911519779bec4dd3eb38184db
Svetoslav Borislavov <svetoslavb1234@gmail.com>
2020-11-01 17:00:56 +0300

    Update on first lesson

--- 01. Programming Basics/First Steps In Coding/First_Steps_In_Coding/07. Fruit Market/Program.cs
+++ 01. Programming Basics/First Steps In Coding/First_Steps_In_Coding/07. Fruit Market/Program.cs
@@ -4,9 +4,21 @@ namespace _07._Fruit_Market
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            double strawberry_price = double.Parse(Console.ReadLine());
+            double bananas = double.Parse(Console.ReadLine());
+            double oranges = double.Parse(Console.ReadLine());
+            double raspberry = double.Parse(Console.ReadLine());
+            double strawberries = double.Parse(Console.ReadLine());
+
+            double raspberry_price = strawberry_price / 2;
+            double orange_price = raspberry_price - (0.4 * raspberry_price);
+            double banana_price = raspberry_price - (0.8 * raspberry_price);
+
+            double required_amonut = bananas * banana_price + oranges * orange_price + raspberry * raspberry_price + strawberries * strawberry_price;
+
+            Console.WriteLine(required_amonut);
         }
     }
 }
--- 01. Programming Basics/First Steps In Coding/First_Steps_In_Coding/08. Fish Tank/Program.cs
+++ 01. Programming Basics/First Steps In Coding/First_Steps_In_Coding/08. Fish Tank/Program.cs
@@ -4,9 +4,18 @@ namespace _08._Fish_Tank
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int length = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine()) / 100;
+
+            double volume = length * width * height;
+            double liters = volume * 0.001;
+            liters = liters - (liters * percent);
+
+            Console.WriteLine(liters);
         }
     }
 }
