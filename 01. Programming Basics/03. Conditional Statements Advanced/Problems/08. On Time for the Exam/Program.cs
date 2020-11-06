452bdc1137295ba0a6bf233bb681785ee2844c84
Svetoslav Borislavov <svetoslavb1234@gmail.com>
2020-11-06 09:40:53 +0200

    Last exam problem and easy one

--- 01. Programming Basics/03. Conditional Statements Advanced/Problems/08. On Time for the Exam/Program.cs
+++ 01. Programming Basics/03. Conditional Statements Advanced/Problems/08. On Time for the Exam/Program.cs
@@ -11,7 +11,65 @@ namespace _08._On_Time_for_the_Exam
             int arrivalHour = int.Parse(Console.ReadLine());
             int arrivalMinutes = int.Parse(Console.ReadLine());
 
-            
+            int examHourInMinutes = examHour * 60;
+            int arrivalHourInMinutes = arrivalHour * 60;
+
+            int examTotalMinutes = examHourInMinutes + examMinutes;
+            int arrivalTotalMinutes = arrivalHourInMinutes + arrivalMinutes;
+
+            if (examTotalMinutes >= arrivalTotalMinutes)
+            {
+                if (examTotalMinutes - arrivalTotalMinutes < 60 && examTotalMinutes - arrivalTotalMinutes >= 0)
+                {
+                    if (examTotalMinutes - arrivalTotalMinutes == 0)
+                    {
+                        Console.WriteLine("On time");
+                    }
+                    else if ((examTotalMinutes - arrivalTotalMinutes) % 60 <= 30 && (examTotalMinutes - arrivalTotalMinutes) % 60 >= 0)
+                    {
+                        Console.WriteLine("On time");
+                        Console.WriteLine($"{(examTotalMinutes - arrivalTotalMinutes) % 60} minutes before the start");
+                    }
+                    else if (examTotalMinutes - arrivalTotalMinutes > 30)
+                    {
+                        Console.WriteLine("Early");
+                        Console.WriteLine($"{(examTotalMinutes - arrivalTotalMinutes) % 60} minutes before the start");
+                    }
+                }
+                else if (examTotalMinutes - arrivalTotalMinutes >= 60) // 9 40 6 55 580 415
+                {
+                    Console.WriteLine("Early");
+
+                    if ((examTotalMinutes - arrivalTotalMinutes) % 60 >= 10) 
+                    {
+                        Console.WriteLine($"{(examTotalMinutes - arrivalTotalMinutes) / 60}:{(examTotalMinutes - arrivalTotalMinutes) % 60} hours before the start");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{(examTotalMinutes - arrivalTotalMinutes) / 60}:0{(examTotalMinutes - arrivalTotalMinutes) % 60} hours before the start");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Late"); 
+
+                if (arrivalTotalMinutes - examTotalMinutes < 60  && arrivalTotalMinutes - examTotalMinutes >= 0) 
+                {
+                    Console.WriteLine($"{arrivalTotalMinutes - examTotalMinutes} minutes after the start");
+                }
+                else
+                {
+                    if ((arrivalTotalMinutes - examTotalMinutes) % 60 >= 10)
+                    {
+                        Console.WriteLine($"{(arrivalTotalMinutes - examTotalMinutes) / 60}:{(arrivalTotalMinutes - examTotalMinutes) % 60} hours after the start");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{(arrivalTotalMinutes - examTotalMinutes) / 60}:0{(arrivalTotalMinutes - examTotalMinutes) % 60} hours after the start");
+                    }
+                }
+            }
         }
     }
 }
---
+++ 01. Programming Basics/03. Conditional Statements Advanced/Problems/9. Volleyball/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _9._Volleyball
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string typeYear = Console.ReadLine(); //leap or normal
+            double holidays = double.Parse(Console.ReadLine());
+            int weekendsToTravelHome = int.Parse(Console.ReadLine());
+
+            double weekendsInSofia = 48 - weekendsToTravelHome;
+
+            double totalVolleybalMatches = 0;
+
+            totalVolleybalMatches = totalVolleybalMatches + weekendsInSofia * 3 / 4;
+            totalVolleybalMatches = totalVolleybalMatches + weekendsToTravelHome;
+            totalVolleybalMatches = totalVolleybalMatches + holidays * 2 / 3;
+
+            if (typeYear == "leap")
+            {
+                totalVolleybalMatches = totalVolleybalMatches + totalVolleybalMatches * 0.15;
+            }
+
+            Console.WriteLine(Math.Floor(totalVolleybalMatches));
+        }
+    }
+}
