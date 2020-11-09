9d20848937894575cefc00b949cb71a57e620db8
Svetoslav Borislavov <svetoslavb1234@gmail.com>
2020-11-09 11:03:36 +0200

    finishing loops

--- 01. Programming Basics/04. For- loop/Problem/05. Divide Without Remainder/Program.cs
+++ 01. Programming Basics/04. For- loop/Problem/05. Divide Without Remainder/Program.cs
@@ -8,7 +8,35 @@ namespace _05._Divide_Without_Remainder
         {
             int n = int.Parse(Console.ReadLine());
 
+            double dividableByTwo = 0;
+            double dividableByThree = 0;
+            double dividableByFour = 0;
 
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number % 2 == 0)
+                {
+                    dividableByTwo++;
+                }
+                if (number % 3 == 0)
+                {
+                    dividableByThree++;
+                }
+                if (number % 4 == 0)
+                {
+                    dividableByFour++;
+                }
+            }
+
+            double percentDividableByTwo = dividableByTwo * 100 / n;
+            double percentDividableByThree = dividableByThree * 100 / n;
+            double percentDividableByFour = dividableByFour * 100 / n;
+
+            Console.WriteLine($"{percentDividableByTwo:F2}%");
+            Console.WriteLine($"{percentDividableByThree:F2}%");
+            Console.WriteLine($"{percentDividableByFour:F2}%");
         }
     }
 }
---
+++ 01. Programming Basics/04. For- loop/Problem/06. Salary/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _06._Salary
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int openedTabs = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < openedTabs; i++)
+            {
+                string tab = Console.ReadLine();
+
+                if (tab == "Facebook")
+                {
+                    salary -= 150;
+                }
+                else if (tab == "Instagram")
+                {
+                    salary -= 100;
+                }
+                else if (tab == "Reddit")
+                {
+                    salary -= 50;
+                }
+
+                if (salary <= 0)
+                {
+                    Console.WriteLine("You have lost your salary.");
+                    return;
+                }
+            }
+
+            Console.WriteLine(salary);
+        }
+    }
+}
