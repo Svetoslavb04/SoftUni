d6cef7d67b25cc5c874500437dae856f4ec0b1f0
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2021-04-27 18:10:48 +0300

    Defining classes exercise the hardest

---
+++ 03. NEW C# Advanced NEW/06. Defining Classes/Exercise/SoftUniParking/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniParking
+{
+    public class Car
+    {
+        public Car(string make, string model, int horsepower, string registrationNumber)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.HorsePower = horsepower;
+            this.RegistrationNumber = registrationNumber;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int HorsePower { get; set; }
+        public string RegistrationNumber { get; set; }
+
+        public override string ToString()
+        {
+            string result = $"Make: {this.Make}{Environment.NewLine}";
+            result += $"Model: {this.Model}{Environment.NewLine}";
+            result += $"HorsePower: {this.HorsePower}{Environment.NewLine}";
+            result += $"RegistrationNumber: {this.RegistrationNumber}";
+
+            return result;
+        }
+    }
+}
---
+++ 03. NEW C# Advanced NEW/06. Defining Classes/Exercise/SoftUniParking/Parking.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+        private Dictionary<string, Car> cars;
+        private int capacity;
+        private int count;
+
+        public Parking(int capacity)
+        {
+            this.Cars = new Dictionary<string, Car>();
+            this.Capacity = capacity;
+            this.count = 0;
+        }
+
+        public Dictionary<string, Car> Cars { get { return this.cars;  } set { this.cars = value; } }
+        public int Capacity { get { return this.capacity; } set { this.capacity = value; } }
+        public int Count { get { return this.count; } set { this.count = value; } }
+
+        public string AddCar(Car car)
+        {
+            if (this.Cars.ContainsKey(car.RegistrationNumber))
+            {
+                return "Car with that registration number, already exists!";
+            }
+            if (this.capacity == count)
+            {
+                return "Parking is full!";
+            }
+            else
+            {
+                this.Cars.Add(car.RegistrationNumber, car);
+                count++;
+
+                return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+            }
+        }
+
+        public string RemoveCar(string RegistrationNumber)
+        {
+            if (!cars.ContainsKey(RegistrationNumber))
+            {
+                return "Car with that registration number, doesn't exist!";
+            }
+            else
+            {
+                cars.Remove(RegistrationNumber);
+                this.count--;
+                return $"Successfully removed {RegistrationNumber}";
+            }
+        }
+
+        public Car GetCar(string RegistrationNumber)
+        {
+            return cars[RegistrationNumber];
+        }
+
+        public void RemoveSetOfRegistrationNumber(List<string> RegistrationNumbers)
+        {
+            for (int i = 0; i < RegistrationNumbers.Count; i++)
+            {
+                if (cars.ContainsKey(RegistrationNumbers[i]))
+                {
+                    cars.Remove(RegistrationNumbers[i]);
+                    this.count--;
+                }
+            }
+        }
+    }
+}
