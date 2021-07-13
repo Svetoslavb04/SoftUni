757d3acbb905b6ff58f31f1f1bfc1a11d91aa126
unknown <39881430+Svetoslavb04@users.noreply.github.com>
2021-07-13 10:27:13 +0200

    OOP Exercise

---
+++ 04. C# OOP/01. Inheritance/Exercise/Solution/Restaurant/Beverage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Beverage : Product
+    {
+        public Beverage(string name, decimal price, double milliliters)
+            :base(name,price)
+        {
+            this.Mililiters = milliliters;
+        }
+
+        public double Mililiters { get; private set; }
+    }
+}
---
+++ 04. C# OOP/01. Inheritance/Exercise/Solution/Restaurant/Coffee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const double mililiters = 50;
+        private const decimal price = 3.5m;
+
+        public Coffee(string name, double caffeine)
+            : base(name, price, mililiters)
+        {
+            Caffeine = caffeine;
+        }
+
+        public double Caffeine { get; set; }
+    }
+}
