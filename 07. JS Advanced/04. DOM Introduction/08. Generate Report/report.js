6c1c72ebf68772498dd5ad0979aa5c7ed4127f84
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-01-24 19:02:45 +0200

    Last problem solved

--- 07. JS Advanced/04. DOM Introduction/08. Generate Report/report.js
+++ 07. JS Advanced/04. DOM Introduction/08. Generate Report/report.js
@@ -1,3 +1,23 @@
 function generateReport() {
-    //TODO
+    let checkboxElements = document.querySelectorAll('thead input');
+    let checkboxes = Array.from(checkboxElements);
+    
+    let rows = [];
+
+    let rowsCount = Array.from(document.querySelectorAll('tbody tr')).length;
+
+    for (let i = 0; i < rowsCount; i++) {
+        rows[i] = {};
+
+        for (let j = 0; j < checkboxes.length; j++) {
+            if (checkboxes[j].checked) {
+                let contentElement = document.querySelector(`tbody tr:nth-child(${i+1}) td:nth-child(${j+1})`);
+                let content = contentElement.textContent;
+                rows[i][checkboxes[j].name] = content;
+            }
+        }
+    }
+
+    let textAreaElement = document.querySelector('textarea');
+    textAreaElement.value = JSON.stringify(rows);
 }
--- 07. JS Advanced/04. DOM Introduction/09. Number Convertor/solution.js
+++ 07. JS Advanced/04. DOM Introduction/09. Number Convertor/solution.js
@@ -1,4 +1,34 @@
 function solve() {
+    let menuElement = document.querySelector('#selectMenuTo');
 
-    //TODO...
+    let binaryOption = document.createElement('option');
+    binaryOption.value = 'binary';
+    binaryOption.textContent = 'Binary';
+    menuElement.appendChild(binaryOption);
+
+    let hexdecimalOption = document.createElement('option');
+    hexdecimalOption.value = 'hexadecimal';
+    hexdecimalOption.textContent = 'Hexadecimal';
+
+    menuElement.appendChild(hexdecimalOption);
+
+    let convertNumber = function () {
+        let numberElement = document.querySelector('#input');
+        let number = Number(numberElement.value);
+
+        let output;
+
+        if (menuElement.value == 'binary') {
+            output = number.toString(2);
+        } else if (menuElement.value == 'hexadecimal') {
+            output = number.toString(16).toUpperCase();
+        }
+
+        let resultElement = document.querySelector('#result');
+        resultElement.value = output;
+    }
+
+    let button = document.querySelector('#container button');
+    button.onclick = convertNumber;
+
 }
