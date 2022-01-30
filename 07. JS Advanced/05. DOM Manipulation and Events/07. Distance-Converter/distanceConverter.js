f5d73e8b6c6249a57bae1a3e919c1579a4237634
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-01-30 20:03:30 +0200

    DOM Manipulations Every Problem

--- 07. JS Advanced/05. DOM Manipulation and Events/07. Distance-Converter/distanceConverter.js
+++ 07. JS Advanced/05. DOM Manipulation and Events/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,74 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    let convertButtonElement = document.querySelector('#convert');
+
+    function convertHandler() {
+        let inputUnit = document.querySelector('#inputUnits').value;
+        let outputUnit = document.querySelector('#outputUnits').value;
+
+        let inputDistance = Number(document.querySelector('#inputDistance').value);
+        let outputDistanceElement = document.querySelector('#outputDistance');
+
+        let inputInMeters = inputDistance;
+
+        switch (inputUnit) {
+            case 'km':
+                inputInMeters *= 1000;
+                break;
+            case 'm':
+                break;
+                
+            case 'cm':
+                inputInMeters /= 100;
+                break;
+            case 'mm':
+                inputInMeters = inputInMeters / 100 / 10;
+                break;
+            case 'mi':
+                inputInMeters *= 1609.34;
+                break;
+            case 'yrd':
+                inputInMeters *= 0.9144;
+                break;
+            case 'ft':
+                inputInMeters *= 0.3048;
+                break;
+            case 'in':
+                inputInMeters *= 0.0254;
+                break;
+            default:
+                break;
+        }
+
+        switch (outputUnit) {
+            case 'km':
+                inputInMeters /= 1000;
+                break;
+            case 'm':
+                break;
+            case 'cm':
+                inputInMeters *= 100;
+                break;
+            case 'mm':
+                inputInMeters = inputInMeters * 100 * 10;
+                break;
+            case 'mi':
+                inputInMeters /= 1609.34;
+                break;
+            case 'yrd':
+                inputInMeters /= 0.9144;
+                break;
+            case 'ft':
+                inputInMeters /= 0.3048;
+                break;
+            case 'in':
+                inputInMeters /= 0.0254;
+                break;
+            default:
+                break;
+        }
+
+        outputDistanceElement.value = inputInMeters;
+    }
+
+    convertButtonElement.addEventListener('click', convertHandler)
 }
