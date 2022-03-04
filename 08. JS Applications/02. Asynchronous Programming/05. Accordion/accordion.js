7f485c13d7e8b27dd18f0906d3c4701f90faa208
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-03-04 18:32:25 +0200

    Finished Homework

--- 08. JS Applications/02. Asynchronous Programming/05. Accordion/accordion.js
+++ 08. JS Applications/02. Asynchronous Programming/05. Accordion/accordion.js
@@ -15,29 +15,47 @@ async function solution() {
     console.log(articles);
     articles.forEach(article => {
         let accordionDiv = document.createElement('div');
+        accordionDiv.classList.add('accordion');
 
-        accordionDiv.innerHTML = `<div class="accordion">
-        <div class="head">
+        accordionDiv.innerHTML = `<div class="head">
             <span>${article['title']}</span>
             <button class="button" id="${article['_id']}">More</button>
         </div>
         <div class="extra">
             <p></p>
-        </div>
-    </div>`;
+        </div>`;
 
     document.getElementById('main')
         .appendChild(accordionDiv);
-    });
+
+    accordionDiv.children[1].style.display = 'none';
 
     async function showInfo() {
-        
+        try {
+            if (accordionDiv.children[0].children[1].textContent == 'More') {
+                let extra = await fetch(`http://localhost:3030/jsonstore/advanced/articles/details/${article['_id']}`)
+                .then((res) => res.json())
+                .then((data) => data);
+
+                accordionDiv.children[1].children[0].textContent = extra['content'];
+
+                accordionDiv.children[0].children[1].textContent = 'Less';
+                accordionDiv.children[1].style.display = 'inline';
+            } else {
+                accordionDiv.children[1].style.display = 'none';
+                accordionDiv.children[0].children[1].textContent = 'More';
+            }
+                
+        } catch (error) {
+            
+        }
     }
 
     accordionDiv
-        .children[1]
         .children[0]
-        .addEventListener('click', )
+        .children[1]
+        .addEventListener('click', showInfo);
+    });
 }
 
 solution();
--- 08. JS Applications/02. Asynchronous Programming/06.Blog/app.js
+++ 08. JS Applications/02. Asynchronous Programming/06.Blog/app.js
@@ -1,5 +1,80 @@
 function attachEvents() {
-    console.log('TODO...');
+    let loadButtonElement = document.getElementById('btnLoadPosts');
+    let viewButtonElement = document.getElementById('btnViewPost');
+
+    loadButtonElement.addEventListener('click', loadPosts);
+    viewButtonElement.addEventListener('click', viewComments);
+
+    let allPostsData = [];
+
+    async function loadPosts() {
+        try {
+            allPostsData = await fetch('http://localhost:3030/jsonstore/blog/posts')
+            .then((res) => res.json())
+            .then(function (data) {
+                let postsArray = [];
+                Array.from(Object.keys(data)).forEach(k => {
+                    postsArray.push({
+                       id: data[k]['id'],
+                       title: data[k]['title'],
+                       body: data[k]['body']
+                    });
+                });
+
+                return postsArray;
+            });
+
+            let allPostsElement = document.getElementById('posts');
+
+            allPostsData.forEach(p => {
+                let optionElement = document.createElement('option');
+                optionElement.value = p.id;
+                optionElement.textContent = p.title;
+
+                allPostsElement.appendChild(optionElement);
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async function viewComments() {
+        try {
+            let selectedOptionId = document.getElementById('posts')
+                .value;
+
+            let allCommentsForCurrentPost = await fetch('http://localhost:3030/jsonstore/blog/comments/')
+                .then((res) => res.json())
+                .then(function(data) {
+                    let posts = [];
+
+                    Array.from(Object.keys(data)).forEach(k => {
+                        if (data[k].postId == selectedOptionId) {
+                            posts.push(data[k].text);
+                        }
+                    });
+
+                    return posts;
+                });
+            
+            document.getElementById('post-title').textContent = allPostsData.find(p => p.id == selectedOptionId).title;
+            document.getElementById('post-body').textContent = allPostsData.find(p => p.id == selectedOptionId).body;
+            
+            let commentsElement = document.getElementById('post-comments');
+            while (commentsElement.firstChild) {
+                commentsElement.removeChild(commentsElement.firstChild);
+            }
+        
+            allCommentsForCurrentPost.forEach(c => {
+                let liElement = document.createElement('li');
+                liElement.textContent = c;
+
+                commentsElement.appendChild(liElement);
+            });
+        } catch (error) {
+            
+        }
+    }
 }
 
 attachEvents();
