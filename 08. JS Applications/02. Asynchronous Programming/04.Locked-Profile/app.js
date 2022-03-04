e40fbdd7d508895e0b2e6147247ab4fa00e4f023
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-03-04 10:43:46 +0200

    Homework

--- 08. JS Applications/02. Asynchronous Programming/04.Locked-Profile/app.js
+++ 08. JS Applications/02. Asynchronous Programming/04.Locked-Profile/app.js
@@ -1,3 +1,82 @@
-function lockedProfile() {
-    console.log('TODO...')
+async function lockedProfile() {
+
+    let users = await getUsers();
+
+    async function getUsers() {
+        try {
+            let usersObj = await fetch('http://localhost:3030/jsonstore/advanced/profiles')
+                            .then((res) => res.json())
+                            .then((data) => data);
+
+            let usersArr = [];
+
+            for (const userKey of Object.keys(usersObj)) {
+                usersArr.push({
+                username: usersObj[userKey].username,
+                email: usersObj[userKey].email,
+                age: usersObj[userKey].age
+                });
+
+            }
+
+            return usersArr;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    for (const user of users) {
+        let cardDiv = document.createElement('div');
+        cardDiv.classList.add('profile');
+
+        cardDiv.innerHTML = `<img src="./iconProfile2.png" class="userIcon" />
+        <label>Lock</label>
+        <input type="radio" name="user1Locked" value="lock" checked>
+        <label>Unlock</label>
+        <input type="radio" name="user1Locked" value="unlock"><br>
+        <hr>
+        <label>Username</label>
+        <input type="text" name="user1Username" value="${user.username}" disabled readonly />
+        <div class="hiddenInfo">
+            <hr>
+            <label>Email:</label>
+            <input type="email" name="user1Email" value="${user.email}" disabled readonly />
+            <label>Age:</label>
+            <input type="email" name="user1Age" value="${user.age}" disabled readonly />
+        </div>
+        
+        <button>Show more</button>`;
+
+        document.getElementById('main')
+            .appendChild(cardDiv);
+
+        cardDiv.children[2].checked = true;
+
+
+        cardDiv.lastChild
+            .addEventListener('click', (e) => {
+                if(e.target.parentNode.children[2].checked) {
+
+                    return;
+        
+                }
+                
+                if(e.target.textContent === 'Show more') {
+                    
+                    e.target.parentNode.children[9].style.display = 'block';
+                    e.target.parentNode.children[9].classList.remove('hiddenInfo');
+                    e.target.parentNode.children[9].children[2].style.display = 'block';
+                    e.target.parentNode.children[9].children[3].style.display = 'block';
+                    e.target.textContent = 'Hide it';
+        
+                } else {
+        
+                    e.target.parentNode.children[9].style.display = 'none';
+                    e.target.parentNode.children[9].classList.add('hiddenInfo');
+        
+                    e.target.textContent = 'Show more'
+        
+                }
+            })
+    }
 }
--- 08. JS Applications/02. Asynchronous Programming/05. Accordion/accordion.js
+++ 08. JS Applications/02. Asynchronous Programming/05. Accordion/accordion.js
@@ -1,3 +1,43 @@
-function solution() {
-    //TODO .....
+async function solution() {
+    var articles = await getArticles();
+
+    async function getArticles() {
+        try {
+            let articles = await fetch('http://localhost:3030/jsonstore/advanced/articles/list')
+                .then((res) => res.json())
+                .then((data) => data);
+
+                return articles;
+        } catch (error) {
+            
+        }
+    }
+    console.log(articles);
+    articles.forEach(article => {
+        let accordionDiv = document.createElement('div');
+
+        accordionDiv.innerHTML = `<div class="accordion">
+        <div class="head">
+            <span>${article['title']}</span>
+            <button class="button" id="${article['_id']}">More</button>
+        </div>
+        <div class="extra">
+            <p></p>
+        </div>
+    </div>`;
+
+    document.getElementById('main')
+        .appendChild(accordionDiv);
+    });
+
+    async function showInfo() {
+        
+    }
+
+    accordionDiv
+        .children[1]
+        .children[0]
+        .addEventListener('click', )
 }
+
+solution();
