3693507794777f31f5c2b86891469befc171e28f
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-03-11 10:44:14 +0200

    SPA Features

--- 08. JS Applications/04. Single Page Applications/02.Movies/src/services/movieService.js
+++ 08. JS Applications/04. Single Page Applications/02.Movies/src/services/movieService.js
@@ -1,5 +1,5 @@
 import { displayDetails } from '../pages/details.js';
-import { _id } from '../auth.js';
+import { authToken, _id } from '../auth.js';
 
 export async function getAllMoviesPreviewElements() {
     let moviesElements = [];
@@ -31,6 +31,7 @@ export async function getMovieDetailsElement(movie_id) {
     
     const containerDiv = document.createElement('div');
     containerDiv.classList.add('container');
+    containerDiv.setAttribute('movie_id', movie_id);
 
     //Data from server is valid
     containerDiv.innerHTML = `<div class="row bg-light text-dark">
@@ -46,27 +47,124 @@ export async function getMovieDetailsElement(movie_id) {
     
     const buttonsParent = containerDiv.children[0].children[2];
 
+    const likes = await fetch(`http://localhost:3030/data/likes?where=movieId%3D%22${movie_id}%22&distinct=_ownerId&count `, {
+            method: 'GET',
+            headers: {
+                'X-Authorization': authToken
+            }})
+            .then(res => res.json())
+            .then(data => {
+                return data;
+            });
+
     if (movie._ownerId == _id) {
         buttonsParent.innerHTML += `<a class="btn btn-danger deleteBtn" href="#">Delete</a>
         <a class="btn btn-warning editBtn" href="#">Edit</a>
-        <span class="enrolled-span likedSpan">Liked 1</span>
+        <span class="enrolled-span likedSpan">Liked ${likes}</span>
         `;
+
+        const editButton = containerDiv.querySelector('.editBtn');
+        const deleteButton = containerDiv.querySelector('.deleteBtn');
     } else {
-        buttonsParent.innerHTML += `<a class="btn btn-primary likeBtn" href="#">Like</a>`;
+        const res = await fetch(`http://localhost:3030/data/likes?where=movieId%3D%22${movie_id}%22%20and%20_ownerId%3D%22${_id}%22`, {
+            method: 'GET',
+            headers: {
+                'X-Authorization': authToken
+            }});
+
+        const data = res.json();
+        if (data.length == 0) {
+            buttonsParent.innerHTML += `<a class="btn btn-primary likeBtn" href="#">Like</a>`;
+            const likeButton = containerDiv.querySelector('.likeBtn');
+
+            likeButton.addEventListener('click', likeMovie);
+        }
+        else {
+            buttonsParent.innerHTML += `<a class="btn btn-primary likedBtn" href="#">Liked ${likes}</a>`;
+        }
     }
 
-    let editButton = containerDiv.querySelector('.editBtn');
-    let deleteButton = containerDiv.querySelector('.deleteBtn');
-    let likeButton = containerDiv.querySelector('.likeBtn');
-    let likedSpan = containerDiv.querySelector('.likedSpan');
-
-    likeButton.addEventListener('click', likeMovie);
+    
+    const likedSpan = containerDiv.querySelector('.likedSpan');
 
     return containerDiv;
 }
 
-function likeMovie() {
-    
+async function likeMovie(e) {
+    const likeButton = e.currentTarget
+    const movieId = likeButton.parentNode.parentNode.parentNode.getAttribute('movie_id');
+
+    try {
+        await fetch('http://localhost:3030/data/likes', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'X-Authorization': authToken
+            },
+            body: JSON.stringify({
+                movieId
+            })
+        });
+
+        const likes = await fetch(`http://localhost:3030/data/likes?where=movieId%3D%22${movieId}%22&distinct=_ownerId&count `, {
+            method: 'GET',
+            headers: {
+                'X-Authorization': authToken
+            }})
+            .then(res => res.json())
+            .then(data => {
+                return data.length;
+            });
+
+        const likedSpan = document.createElement('span');
+        likedSpan.classList.add('enrolled-span', 'likedSpan');
+        likedSpan.textContent = `Liked ${likes}`;
+        likedSpan.addEventListener('click', dislikeMovie);
+
+        likeButton.parentNode.appendChild(likedSpan);
+
+        likeButton.remove();
+
+    } catch (error) {
+        alert(error.message);
+    }
+}
+
+async function dislikeMovie(e) {
+    const likedSpan = e.currentTarget
+    const movieId = likedSpan.parentNode.parentNode.parentNode.getAttribute('movie_id');
+
+    try {
+        const likeId = await fetch(`http://localhost:3030/data/likes?where=movieId%3D%22${movieId}%22%20and%20_ownerId%3D%22${_id}%22`, {
+            method: 'GET',
+            headers: {
+                'X-Authorization': authToken
+            }})
+            .then((res) => res.json())
+            .then((data) => {
+                return data[0]._id;
+            });
+
+        console.log(likeId);
+
+        await fetch(`http://localhost:3030/data/likes/${likeId}`, {
+            method: 'DELETE',
+            headers: {
+                'X-Authorization': authToken
+            }});
+
+        const likeButton = document.createElement('a');
+        likeButton.classList.add('btn' ,'btn-primary', 'likeBtn');
+        likeButton.textContent = `Like`;
+        likeButton.addEventListener('click', likeMovie);
+
+        likedSpan.parentNode.appendChild(likeButton);
+
+        likedSpan.remove();
+
+    } catch (error) {
+        alert(error.message);
+    }
 }
 
 function createMoviePreviewElement(movie) {
