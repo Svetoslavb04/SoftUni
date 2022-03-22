f23f41e7650230ff7a6088bb10efdf3fc791c6e4
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-03-22 18:22:48 +0200

    Basic server without Express completed
    Written the server, BUT without any good practices

--- 09. Intro to Node.js/Cat Shelter/index.js
+++ 09. Intro to Node.js/Cat Shelter/index.js
@@ -4,6 +4,7 @@ const port = 3030;
 const path = require('path');
 const fs = require('fs');
 const formidable = require('formidable');
+const url = require('url');
 
 let cats = require('./data/cats.json');
 let breeds = require('./data/breeds.json');
@@ -19,17 +20,26 @@ http.createServer((req, res) => {
     let reveresedId = reversedPath.slice(0, indexOfSlash);
     let id = Array.from(reveresedId).reverse().join('');
 
+    const queryObject = url.parse(req.url, true).query;
+
     switch (pathName) {
         case `/content/images/${id}`:
             filePath = path.normalize(
                 path.join(__dirname, `/content/images/${id}`)
             );
-            
+
+            let mimeType = 'image/';
+
+            if (id.endsWith('jpg') || id.endsWith('jpeg') || id.endsWith('jpe')) {
+                mimeType += 'jpeg';
+            } else if (id.endsWith('png') || id.endsWith('ico')) { mimeType += 'png'}
+            else if (id.endsWith('gif')) { mimeType += 'gif' }
+
             res.writeHead(200, {
-                'Content-type': 'image/jpeg'
+                'Content-Type': mimeType
             });
 
-            body = fs.createReadStream(filePath, 'utf8');
+            body = fs.createReadStream(filePath);
             body.pipe(res);
             break;
         case '/content/styles/site.css':
@@ -69,7 +79,7 @@ http.createServer((req, res) => {
                             <p><span>Description: </span>${c.description}</p>
                             <ul class="buttons">
                                 <li class="btn edit"><a href="/cat-edit/${c.id}">Change Info</a></li>
-                                <li class="btn delete"><a href="/cat-find-new-home">New Home</a></li>
+                                <li class="btn delete"><a href="/cat-find-new-home/${c.id}">New Home</a></li>
                             </ul>
                         </li>
                     `)
@@ -224,8 +234,7 @@ http.createServer((req, res) => {
                             breedsArr.splice(indexOfBreed, 1);
                             breedsArr.unshift(cats[index].breed);
 
-                            data = data.replace(`<form action="" method="POST" class="cat-form" enctype="multipart/form-data">`, `<form action="/cat-edit/${id}" method="POST" class="cat-form" enctype="multipart/form-data">`);
-                            data = data.replace('<input type="text" id="name" value="{{NAME}}">', `<input type="text" id="name" value="${name}">`);
+                            data = data.replace('<input name="name" type="text" id="name" value="{{NAME}}">', `<input name="name" type="text" id="name" value="${name}">`);
                             data = data.replace('{{DESCRIPTION}}', description);
                             data = data.replace('{{BREEDS}}', breedsArr.map(b => `<option value="${b}">${b}</option>`));
 
@@ -235,11 +244,13 @@ http.createServer((req, res) => {
                     });
                 })
             } else if(req.method == 'POST') {
-                let form = new formidable.IncomingForm();
+                let editform = new formidable.IncomingForm();
                 
-                let cat = {};
+                let editedcat = {};
 
-                form.parse(req, (err, fields, files) => {
+                editform.parse(req, (err, fields, files) => {
+                    if (err) return;
+                    
                     if (files.upload.originalFilename != '') {
                         let oldPath = files.upload.filepath;
                         let newPath = path.normalize( path.join(__dirname, `./content/images/${files.upload.newFilename}${files.upload.originalFilename}`));
@@ -248,14 +259,14 @@ http.createServer((req, res) => {
                         if (err) {
                             return;
                         }
-                        cat.image = `./content/images/${files.upload.newFilename}${files.upload.originalFilename}`;
+                        editedcat.image = `./content/images/${files.upload.newFilename}${files.upload.originalFilename}`;
 
                         });
-                    } else { cat.image = null; }
+                    } else { editedcat.image = null; }
                     
-                    cat.name = fields.name;
-                    cat.description = fields.description;
-                    cat.breed = fields.breed;
+                    editedcat.name = fields.name;
+                    editedcat.description = fields.description;
+                    editedcat.breed = fields.breed;
 
                     let dataFilePath = path.normalize( path.join(__dirname, './data/cats.json'));
 
@@ -263,8 +274,11 @@ http.createServer((req, res) => {
                         if (err) return;
                         
                         let currentData = JSON.parse(data);
-                        cat.id = currentData.length;
-                        currentData.push(cat);
+                        editedcat.id = id;
+
+                        let indexOfCat = currentData.findIndex(c => c.id == id);
+
+                        currentData.splice(indexOfCat, 1, editedcat);
 
                         const stream = fs.createWriteStream(dataFilePath, 'utf8');
                         stream.write(JSON.stringify(currentData, '', 2));
@@ -279,15 +293,116 @@ http.createServer((req, res) => {
                 });
             }
             break;
-        default: 
-            res.writeHead(404, {
-                'Content-Type': 'text/plain'
+        case `/cat-find-new-home/${id}`:
+            if (req.method == 'GET') {
+                let filePath = path.normalize( path.join(__dirname, './views/catShelter.html'));
+
+                fs.readFile(filePath, 'utf8', (err, data) => {
+                    if(err) return;
+
+                    let dataFilePath = path.normalize( path.join(__dirname, './data/cats.json'));
+
+                    fs.readFile(dataFilePath, 'utf8', (err, catData) => {
+                        if (err) return;
+
+                        let cats = JSON.parse(catData);
+                    
+                        let index = cats.findIndex(c => c.id == id);
+
+                        let name = cats[index].name;
+                        let description = cats[index].description;
+                        let breed = cats[index].breed;
+                        let image = cats[index].image;
+
+                        data = data.replace('<img src="" alt="Cat">', `<img src=".${image}" alt="Cat">`)
+                        data = data.replace('<input type="text" id="name" value="Pretty Kitty" disabled>', `<input type="text" id="name" value="${name}" disabled>`);
+                        data = data.replace('{{DESCRIPTION}}', description);
+                        data = data.replace('{{BREEDS}}', `<option value="${breed}">${breed}</option>`);
+
+                        res.write(data);
+                        res.end();
+                    });
+                })
+            } else {
+                let dataFilePath = path.normalize( path.join(__dirname, './data/cats.json'));
+
+                    fs.readFile(dataFilePath, 'utf8', (err, data) => {
+                        if (err) return;
+                        
+                        let currentData = JSON.parse(data);
+
+                        let indexOfCat = currentData.findIndex(c => c.id == id);
+
+                        currentData.splice(indexOfCat, 1);
+
+                        const stream = fs.createWriteStream(dataFilePath, 'utf8');
+                        stream.write(JSON.stringify(currentData, '', 2));
+                        stream.end();
+
+                        res.writeHead(302, {
+                            'Location': '/'
+                        });
+        
+                        res.end();
+                    });
+            }
+            break;
+        case `/search?search=${queryObject.search}`:
+            filePath = path.normalize(
+                path.join(__dirname, './views/home/index.html')
+            );
+            
+            res.writeHead(200, {
+                'Content-type': 'text/html'
             });
 
-            res.write('Sorry, bro! Resource not found');
-            res.end();
+            fs.readFile(filePath, 'utf8', (err, data) => {
+                let dataFilePath = path.normalize( path.join(__dirname, './data/cats.json'));
+                fs.readFile(dataFilePath, 'utf8', (err, cats) => {
+                    if (err) return;
+                        
+                    let catsArr = JSON.parse(cats);
+                    let filteredCats = catsArr.reduce((arr, c) => {
+                        let name = c.name;
+                        let breed = c.breed;
+                        let description = c.description;
+
+                        if (name.includes(queryObject.search) || breed.includes(queryObject.search) || description.includes(queryObject.search)) {
+                            arr.push(c);
+                        }
+                        
+                        return arr;
+                    }, []);
+                    
+                    let mappedCats = filteredCats.map(c => `
+                        <li>
+                            <img src="${c.image}" alt="Cat">
+                            <h3>${c.name}</h3>
+                            <p><span>Breed: </span>${c.breed}</p>
+                            <p><span>Description: </span>${c.description}</p>
+                            <ul class="buttons">
+                                <li class="btn edit"><a href="/cat-edit/${c.id}">Change Info</a></li>
+                                <li class="btn delete"><a href="/cat-find-new-home/${c.id}">New Home</a></li>
+                            </ul>
+                        </li>
+                    `)
+
+                    data = data.replace('{{AllCats}}', mappedCats)
+                    
+                    res.write(data);
+                    res.end();
+                });
+            });
             break;
-    }
+        default: 
+        res.writeHead(404, {
+            'Content-Type': 'text/plain'
+        });
+
+        res.write('Sorry, bro! Resource not found');
+        res.end();
+        break;
+}
 
 }).listen(port);
 
